f2e16c258f57e7cf4d103af4932b991c5036c964
FranciscoPereira <francisco.o.pereira2005@gmail.com>
2025-07-07 08:41:01 +0100

    change media queryes and input js out

--- main.js
+++ main.js
@@ -13,11 +13,25 @@ headers.forEach(header => {
   });
 });
 
+
+
 document.addEventListener('DOMContentLoaded', function() {
   const menuToggle = document.querySelector('.menu-toggle');
   const navbarMenu = document.querySelector('.navbar__menu');
 
-  menuToggle.addEventListener('click', function() {
-    navbarMenu.classList.toggle('active');
+  // abre/fecha menu
+  menuToggle.addEventListener('click', function(e) {
+    navbarMenu.classList.toggle('navbar__menu--open');
+  });
+
+  // fecha quando clicar fora
+  document.addEventListener('click', function(e) {
+    if (
+      navbarMenu.classList.contains('navbar__menu--open') &&
+      !navbarMenu.contains(e.target) &&
+      !menuToggle.contains(e.target)
+    ) {
+      navbarMenu.classList.remove('navbar__menu--open');
+    }
   });
 });
